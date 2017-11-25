202fb93868acdddbeb84d674d8707b6bcb3cba41
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-11-25 00:53:38 -0500

    comments

--- assets/js/containers/Landing.js
+++ assets/js/containers/Landing.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Row, Col } from 'react-grid-system';
 import { FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap';
-
+import { joinAppGeneral } from '../actions/app';
 
 //TODO handle submits
+  // app joins a general channel?
+    // send CREATE_TABLE/ JOIN TABLE down channel with params
+    // and then join?
+    // on app mount we need to create socket and join channel general
+
+//TODO create channel, table resource
+  // users will join a channel for the table_id
+  // use presence to track users?
+
+  // PRIORITY 1: USERS JOIN TABLES AND chat/ see list of users
+
 class Landing extends React.Component {
   render() {
     return (
@@ -29,7 +40,6 @@ class Landing extends React.Component {
             </Button>
 
             <p>or</p>
-
             <Button type="submit">
               Create Table
             </Button>
